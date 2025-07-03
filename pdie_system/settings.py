8a2c66dfa084f5541b727aaf6581a6980cf0f19a
Hazim <iqwanmdhazim@gmaill.com>
2025-07-04 06:48:27 +0800

    Configure Django settings for production deployment on Render

--- pdie_system/settings.py
+++ pdie_system/settings.py
@@ -21,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(gn#abnknqewr-wzdj2(*7+i3w#ok+n*h=vqi4gt*v4hn*y8tx'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(gn#abnknqewr-wzdj2(*7+i3w#ok+n*h=vqi4gt*v4hn*y8tx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    '127.0.0.1', 
+    'localhost',
+    '.onrender.com',  # Allow all Render subdomains
+    '.render.com',    # Allow Render domains
+]
 
 
 # Application definition
@@ -128,8 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT is for production only. Comment out during development.
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
